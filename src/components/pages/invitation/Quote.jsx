0c3932c232d3732a596e8de03ffdfe88c099239a
Benjamín Garrido <bengarr.negocios@gmail.com>
2023-06-22 13:55:08 -0600

    Fix: Author

--- src/components/pages/invitation/Quote.jsx
+++ src/components/pages/invitation/Quote.jsx
@@ -6,31 +6,40 @@ import React from "react";
 
 
 export default function Quote({quote, author, imageSrc, ...props}) {
-	return (
-		<MobileScreen
-			sx = { { border: '4px solid white', background:'#070020', scrollSnapAlign: 'center'} }
-			direction = { 'column' } >
-			<FlexColumn center sx={{ height:'100%', position:'relative' }} >
-				<FlexColumn center sx={{ position:'absolute', height:'100%', zIndex: 2, textAlign:'end', padding:'32px', background:'rgba(0,0,0,0.4)' }} >
-					<Typography
-						variant = { 'h2' }
-						sx = { {
-							fontWeight: 'bold',
-							color: 'rgba(255,255,255,100)!important',
-							fontFamily: 'Playfair Display',
-							textAlign: 'center',
-							fontSize: '42px',
-							padding:'0 12px',
-							letterSpacing: '.2rem',
-						}} >
-						{quote}
-					</Typography>
-					<Typography>
-						- {author}
-					</Typography>
-				</FlexColumn>
-				<DynamicSizeNextImage src={imageSrc} width={'100%'} height={'100%'} />
-			</FlexColumn>
-		</MobileScreen >
-	);
+    return (
+        <MobileScreen
+            sx={{border: '4px solid white', background: '#070020', scrollSnapAlign: 'center'}}
+            direction={'column'}>
+            <FlexColumn center sx={{height: '100%', position: 'relative'}}>
+                <FlexColumn center sx={{
+                    position: 'absolute',
+                    height: '100%',
+                    zIndex: 2,
+                    textAlign: 'end',
+                    padding: '32px',
+                    background: 'rgba(0,0,0,0.4)'
+                }}>
+                    <Typography
+                        variant={'h2'}
+                        sx={{
+                            fontWeight: 'bold',
+                            color: 'rgba(255,255,255,100)!important',
+                            fontFamily: 'Playfair Display',
+                            textAlign: 'center',
+                            fontSize: '42px',
+                            padding: '0 12px',
+                            letterSpacing: '.2rem',
+                        }}>
+                        {quote}
+                    </Typography>
+                    {
+                        author && <Typography>
+                            - {author}
+                        </Typography>
+                    }
+                </FlexColumn>
+                <DynamicSizeNextImage src={imageSrc} width={'100%'} height={'100%'}/>
+            </FlexColumn>
+        </MobileScreen>
+    );
 }
